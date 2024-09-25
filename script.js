018b8d96e15bee331c2e80eeb610d3073a09b1d2
Portile0Social <highchr+PS@nclack.org>
2024-09-25 01:06:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function showPage(pageId) {
     var contents = document.querySelectorAll('.content');
     contents.forEach(function(content) {
-        content.classList.remove('active');
+        content.classList.remove('active'); // Hide all content sections
     });
 
     setTimeout(function() {
         var currentPage = document.getElementById(pageId);
-        currentPage.classList.add('active');
+        currentPage.classList.add('active'); // Show the selected page
+    }, 10);
+}
 
 // Display the home page by default
 showPage('home');
