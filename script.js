b6963a5a3bfd6f30589bb416c5635f985ba02478
Portile0Social <highchr+PS@nclack.org>
2024-10-16 08:16:46 +1400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('keydown', function(event) {
+        if (event.code === 'Backquote') {
+            event.preventDefault(); 
+            fetch('loginPS.html')
+                .then(response => response.text())
+                .then(html => {
+                    document.body.innerHTML = html;})
+                .catch(error => console.error('Error loading the page:', error));}});});
